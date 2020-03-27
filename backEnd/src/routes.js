40cbaeb2238143e762d82d117bf7c50068b21f65
brunaazambuja <bubu1azambuja@gmail.com>
2020-03-27 14:51:17 -0300

    Desenvolvendo Validações e testes.

--- backEnd/src/routes.js
+++ backEnd/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -7,16 +8,67 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create); /* login de ong */
+
+
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.required()
+    })
+}), SessionController.create); /* login de ong */
+
+
 
 routes.get('/ongs', OngController.index); /* listagem de ongs */
-routes.post('/ongs', OngController.create); /* cadastro de ongs */
 
-routes.get('/profile', ProfileController.index); /* listagem de todos os casos da ong logada */
 
-routes.post('/incidents', IncidentController.create); /* cadastro de casos */
-routes.get('/incidents', IncidentController.index); /* listagem de casos */
-routes.delete('/incidents/:id', IncidentController.delete); /* deleção de caso específico */
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create); /* cadastro de ongs */
+
+
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index); /* listagem de todos os casos da ong logada */
+
+
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string(),
+        value: Joi.number().required()
+    })
+}), celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), IncidentController.create); /* cadastro de casos */
+
+
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index); /* listagem de casos */
+
+
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete); /* deleção de caso específico */
 
 
 module.exports = routes;
